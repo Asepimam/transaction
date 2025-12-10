1fb2a803a8146468808072386230f0c593a06b60
Asepimam <asepimam329@gmail.com>
2025-12-10 10:58:37 +0700

    Add UserRepository for managing User entity data access

---
+++ src/main/java/com/transaction/transaction/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.transaction.transaction.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.transaction.transaction.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
