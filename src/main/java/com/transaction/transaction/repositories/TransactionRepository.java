1e73d1ee66a92b06e09dbdf853bdd2d755c108f7
Asepimam <asepimam329@gmail.com>
2025-12-10 10:58:13 +0700

    Add TransactionRepository for managing Transaction entity data access

---
+++ src/main/java/com/transaction/transaction/repositories/TransactionRepository.java
@@ -0,0 +1,12 @@
+package com.transaction.transaction.repositories;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.transaction.transaction.entities.Transaction;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    Page<Transaction> findByAccountId(Long accountId, Pageable pageable);   
+}
