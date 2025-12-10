172069b311ec33bdf92ef0f24b67365d9d91e488
Asepimam <asepimam329@gmail.com>
2025-12-10 10:57:34 +0700

    Add ResouceExistException for handling resource existence errors

---
+++ src/main/java/com/transaction/transaction/exceptions/ResouceExistException.java
@@ -0,0 +1,8 @@
+package com.transaction.transaction.exceptions;
+
+public class ResouceExistException extends RuntimeException {
+    
+    public ResouceExistException(String message) {
+        super(message);
+    }
+}
