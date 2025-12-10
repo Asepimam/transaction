d89d1b5651ad24668212832cf88f97b8c784cee7
Asepimam <asepimam329@gmail.com>
2025-12-10 10:56:54 +0700

    Add User entity for managing user information and relationships with accounts

---
+++ src/main/java/com/transaction/transaction/entities/User.java
@@ -0,0 +1,29 @@
+package com.transaction.transaction.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+
+    @OneToOne(mappedBy = "user")
+    private Account account;
+}
