f203cea24c0fbbbe3eb1d179e7c4295621685ff5
Asepimam <asepimam329@gmail.com>
2025-12-10 10:56:25 +0700

    Add Transaction entity for managing transaction records

---
+++ src/main/java/com/transaction/transaction/entities/Transaction.java
@@ -0,0 +1,41 @@
+package com.transaction.transaction.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long accountId;
+
+    private Double amount;
+
+    private String type;
+
+    private Long  transferId;
+
+    private String category;
+
+    private String status;
+
+    private String description;
+
+    private LocalDateTime date = LocalDateTime.now();
+}
