d106d018bca0250375f13fd6f63302a288c83a76
Asepimam <asepimam329@gmail.com>
2025-12-10 10:57:42 +0700

    Add ResouceNotFoundException for handling resource not found errors

---
+++ src/main/java/com/transaction/transaction/exceptions/ResouceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.transaction.transaction.exceptions;
+
+public class ResouceNotFoundException extends RuntimeException {
+    public ResouceNotFoundException(String message) {
+        super(message);
+    }
+    
+}
