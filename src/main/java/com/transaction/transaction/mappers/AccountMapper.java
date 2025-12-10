b4c6118c95d3aa58f38c298ba4a05353756c917f
Asepimam <asepimam329@gmail.com>
2025-12-10 13:33:19 +0700

    Refactor AccountMapper to include user mapping for ResponseAccountDto

--- src/main/java/com/transaction/transaction/mappers/AccountMapper.java
+++ src/main/java/com/transaction/transaction/mappers/AccountMapper.java
@@ -1,6 +1,7 @@
 package com.transaction.transaction.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import com.transaction.transaction.dto.ResponseAccountBalanceDto;
@@ -11,8 +12,11 @@ public interface AccountMapper {
 
     AccountMapper INSTANCE = Mappers.getMapper(AccountMapper.class);
 
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "user.userName", target = "userName")
     ResponseAccountDto toResponseAccountDto(com.transaction.transaction.entities.Account account);
     
+    @Mapping(source = "user.id", target = "userId")
     ResponseAccountBalanceDto toResponseAccountBalanceDto(com.transaction.transaction.entities.Account account);
 
     
